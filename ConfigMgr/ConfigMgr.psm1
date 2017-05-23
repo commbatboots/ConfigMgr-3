3efbcd1a18fec468adfe940d87b8f5aed5aab104
r.kooistra_an <r.kooistra_an@AN-HNG6G72>
2017-05-23 22:28:39 +0200

    Initial commit

--- ConfigMgr/ConfigMgr.psm1
+++ ConfigMgr/ConfigMgr.psm1
@@ -1,6 +1,10 @@
-<#
-	My Function
-#>
-function Get-Function {
+<Function Get-WCMSiteCode {
+	[CmdLetBinding()]
+
+	Param(
+		[Parameter(Mandatory=$True)]
+		[ValidateScript({Test-Connection $_ -Count 1 -Quiet})]
+		[String] $ComputerName
+	)
 
 }
