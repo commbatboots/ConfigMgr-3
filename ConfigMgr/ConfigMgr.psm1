a874d5c7d4904f177c13e4a2f0a242699e672735
r.kooistra_an <r.kooistra_an@AN-HNG6G72>
2017-05-23 22:52:07 +0200

    Added New-WCMCollectionVariable

--- ConfigMgr/ConfigMgr.psm1
+++ ConfigMgr/ConfigMgr.psm1
@@ -8,3 +8,82 @@
 	)
 
 }
+
+Function New-WCMCollectionVariable {
+	<#.Synopsis
+	Add a collection variable
+	.DESCRIPTION
+	Add a collection variable to an ConfigMgr collection
+	.EXAMPLE
+	New-WCMCollectionVariable -Name MyCollectionVariable - Value "Example value"
+	.EXAMPLE
+	New-WCMCollectionVariable -Name MyCollectionVariable - Value "Example hidden value" -HideValueInConsole $True
+	#>
+    [CmdLetBinding()]
+    
+    Param(
+        [Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [String] $Name,
+
+        [Parameter(Mandatory=$True,Position=2,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [String] $Value,
+        
+        [ValidateScript({ Test-Connection -ComputerName $_ -Count 1 -Quiet })]
+        [String] $ComputerName = ".",
+
+        [ValidateLength(3,3)]
+        [String] $Site,
+
+        [Parameter(Mandatory=$True)]
+        [String] $CollectionID,
+        
+        [Parameter(Position=3,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [String] $HideValueInConsole = $False
+    )
+
+    Begin {
+        #region Setup
+        If( $ComputerName -ne "." ) {
+            Write-Verbose "Using remote computer '$ComputerName'"
+        }
+
+        If( $Site -eq "" ) {
+            Write-Verbose "No site code was provided, attempt auto detection"
+        
+            Try {
+                $locationProvider = Get-CimInstance -ClassName "SMS_ProviderLocation" -Namespace "Root\SMS" -ComputerName $ComputerName -ErrorAction SilentlyContinue
+            
+                If( ($locationProvider -eq $null) -or ($locationProvider.SiteCode -eq "")) {
+                    Throw [System.Management.Automation.ItemNotFoundException] "No valid sitecode found on $($ComputerName)"
+                }
+                Else {
+                    Write-Verbose "Setting site code to $($locationProvider.SiteCode)"
+                    $Site = $locationProvider.SiteCode
+                }
+            } Catch {
+                Write-Error -Exception $_ -Category InvalidResult
+            }
+        }
+    }
+
+    Process {
+        $objCollectionVariable = ([wmiclass] "\\$ComputerName\root\sms\site_$($Site):SMS_CollectionVariable").CreateInstance()
+        $objCollectionVariable.Name = $Name
+        $objCollectionVariable.Value = $Value
+        $objCollectionVariable.IsMasked = [System.Convert]::ToBoolean($HideValueInConsole)
+    
+        $objCollectionSetting = Get-WmiObject -ComputerName $ComputerName -Class SMS_CollectionSettings -Namespace root\sms\site_$($Site) -Filter "CollectionID = '$collectionID'"
+
+        If( $objCollectionSetting -eq $null ) {
+            Write-Verbose "Collection setting object not found for this collection, creating new instance"
+            $objCollectionSetting = ([wmiclass] "\\$ComputerName\root\sms\site_$($Site):SMS_CollectionSettings").CreateInstance()
+        }
+        Else {
+            $objCollectionSetting.Get()
+        }
+
+        $objCollectionSetting.CollectionVariables += $objCollectionVariable
+        #$objCollectionSetting.Put() | Out-Null
+        #Write-Output "Sucessfully created collection variable $Name with value $Value"
+    }
+}
